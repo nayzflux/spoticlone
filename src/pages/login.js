387d6f839520d185092414c3c75fae7ddb46f854
NayZ <55506519+nayzflux@users.noreply.github.com>
2023-07-11 11:30:44 +0200

    Update login.js
    Adding warnings

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,6 +4,8 @@ function Login({ providers }) {
     return (
         <div className='flex flex-col items-center bg-black min-h-screen w-full justify-center'>
             <img className='w-52 mb-5' src='https://links.papareact.com/9xl' alt='spotify logo'></img>
+            <p className="text-white font-medium">This site is not the official Spotify Website, it's a clone to learn React & Netxt.</p>
+            <a href="https://spotify.com" className="text-blue-600 hover:underline">You search the official site of Spotify? Click Here.</a>
             {Object.values(providers).map((provider) => <div key={provider.name}><button onClick={() => signIn(provider.id, {callbackUrl: "/"})} className='bg-[#18D860] text-white p-5 rounded-full'>Login with {provider.name}</button></div>)}
         </div>
     );
